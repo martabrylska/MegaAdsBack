fe5115b4d6a4877d46c57f6d819c3f0c3b27c8df
Marta Brylska <tomica.marta@gmail.com>
2023-08-28 11:29:46 +0200

    added Jest and first tests to project

---
+++ tests/ad-record.test.ts
@@ -0,0 +1,24 @@
+import {AdRecord} from "../records/ad.record";
+
+const defaultObj = {
+    name: 'Test Name',
+    description: 'blah',
+    url: 'https://megak.pl',
+    price: 0,
+    lat: 9,
+    lon: 9,
+}
+
+test('Can build AdRecord', () => {
+    const ad = new AdRecord(defaultObj);
+
+    expect(ad.name).toBe('Test Name');
+    expect(ad.description).toBe('blah');
+});
+
+test('Validates invalid price', () => {
+    expect(() => new AdRecord({
+        ...defaultObj,
+        price: -3,
+    })).toThrow('Cena nie może być mniejsza od 0 ani większa niż 9 999 999zl');
+})
