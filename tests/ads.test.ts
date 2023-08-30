e8d5e2dff2d4b66f783d6f75506658f00720d195
Marta Brylska <tomica.marta@gmail.com>
2023-08-30 10:46:04 +0200

    added searching and adding of ads.

--- tests/ads.test.ts
+++ tests/ads.test.ts
@@ -1,4 +1,10 @@
 import {AdRecord} from "../records/ad.record";
+import {pool} from "../utils/db";
+import {AdEntity} from "../types";
+
+afterAll(async () => {
+    await pool.end();
+})
 
 test('AdRecord returns data from database for one entry.', async () => {
     const ad = await AdRecord.getOne('abc');
@@ -7,10 +13,45 @@ test('AdRecord returns data from database for one entry.', async () => {
     expect(ad).toBeDefined();
     expect(ad.name).toBe('testowa');
 
-})
+});
 
 test('AdRecord returns null from database for unexisting entry.', async ()=>{
     const ad = await AdRecord.getOne('abcd');
 
     expect(ad).toBeNull();
-})
+});
+
+test('AdRecord returns all data from database.', async ()=>{
+    const ads = await AdRecord.findAll('');
+    expect(ads).toBeDefined();
+});
+
+test('AdRecord returns searched data from database.', async ()=>{
+    const ads = await AdRecord.findAll('test');
+    expect(ads).toBeDefined();
+    expect((ads[0]as AdEntity).description).toBeUndefined();
+    expect((ads[0]as AdEntity).price).toBeUndefined();
+    expect((ads[0]as AdEntity).lat).toBeDefined();
+});
+
+test('AdRecord returns empty array from database for not matching data.', async ()=>{
+    const ads = await AdRecord.findAll('xxx');
+
+    expect(ads).toStrictEqual([])
+});
+
+test('AdRecord returns id when addind to database.', async ()=>{
+    const ad = new AdRecord({
+        name: 'test',
+        description: 'test',
+        price: 0,
+        url: 'test.pl',
+        lat: 3,
+        lon: 4,
+    });
+
+    await ad.insert();
+
+    expect(ad.id).toBeDefined();
+
+});
